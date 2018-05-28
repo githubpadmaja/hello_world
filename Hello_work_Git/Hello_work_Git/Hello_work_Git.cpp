b993eb5bb267b2f672495d7d568935fb4fd3f02c
Padmaja Kuppachi <c_pkuppa@qti.qualcomm.com>
2018-05-28 14:41:23 +0530

    First commit for github training

---
+++ Hello_work_Git/Hello_work_Git/Hello_work_Git.cpp
@@ -0,0 +1,14 @@
+// Hello_work_Git.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <stdio.h>
+
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	std::cout << "Hello word git hub project";
+}
+
